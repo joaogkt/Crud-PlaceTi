8ae0622d2519f674d27599f2432d085428b4eaa6
João Torres <116197527+joaogkt@users.noreply.github.com>
2024-06-08 18:14:54 -0300

    Update Modelo.java

--- src/main/java/org/example/crudplaceti/models/Modelo.java
+++ src/main/java/org/example/crudplaceti/models/Modelo.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Modelo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String nome;
