8d54c1241a18f9d9f0fc7bd50d0c6898c2ba9083
João Torres <116197527+joaogkt@users.noreply.github.com>
2024-06-08 18:14:42 -0300

    Update Marca.java

--- src/main/java/org/example/crudplaceti/models/Marca.java
+++ src/main/java/org/example/crudplaceti/models/Marca.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Entity
 public class Marca {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String nome;
     private String codigoDenatran;
